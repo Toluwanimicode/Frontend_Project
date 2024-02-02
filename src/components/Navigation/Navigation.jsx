7bfb2e28a08cbb10bd3cff3202021a7fcafb06c8
Toluwanimicode <da.toluwanimi@gmail.com>
2024-02-02 18:20:05 +0100

    added front-end

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -18,19 +18,19 @@ function Navigation() {
           <Nav className="me-auto navbar">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <NavDropdown title="About Us" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/about">About</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/schoolAnthem">School Anthem / Motto</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/contact">Contact Us</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/About">About</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/SchoolAnthem">School Anthem / Motto</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/Contact">Contact Us</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link as={Link} to="/admission">Admission</Nav.Link>
+            <Nav.Link as={Link} to="/Admission">Admission</Nav.Link>
             <NavDropdown title="Academics" id="basic-nav-dropdown">
               <NavDropdown.Item as={Link} to="/news">News</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/calendar">Academic Calendar</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/events">Events</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/teachers">Our Teachers</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+            <Nav.Link as={Link} to="/Login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/Gallery">Gallery</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
