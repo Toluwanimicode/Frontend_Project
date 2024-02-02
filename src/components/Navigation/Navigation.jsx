82d82130066e1108fe12cf5929bed0d3288bfc1c
Toluwanimicode <da.toluwanimi@gmail.com>
2024-02-02 18:07:36 +0100

    added front-end

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,33 +1,36 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import "../Navigation/Navigation.css"
-
+import "../Navigation/Navigation.css";
 
 function Navigation() {
   return (
-    <Navbar  expand="lg" className='nav'>
-      <Container >
-        <Navbar.Brand href="#home"> <img  className="image" src={"./images/Gallery/Logo.png"} alt="School Logo" width="500" height="600"></img>MountainTop Schools </Navbar.Brand>
+    <Navbar expand="lg" className='nav'>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img className="image" src={"./images/Gallery/Logo.png"} alt="School Logo" width="500" height="600" />
+          MountainTop Schools
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto navbar">
-            <Nav.Link href="/">Home</Nav.Link>          
-            <NavDropdown href="/academics"  title="About Us" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/About">About</NavDropdown.Item>
-              <NavDropdown.Item href="/SchoolAnthem">School Anthem / Motto</NavDropdown.Item>          
-              <NavDropdown.Item href="/contact">Contact Us</NavDropdown.Item>                       
-            </NavDropdown>       
-            <Nav.Link href="/Admission">Admission</Nav.Link>          
-            <NavDropdown href="/academics"  title="Academics" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/news">News</NavDropdown.Item>
-              <NavDropdown.Item href="/calendar"> Academic Calendar</NavDropdown.Item>          
-              <NavDropdown.Item href="/events">Events</NavDropdown.Item>          
-              <NavDropdown.Item href="/teachers">Our Teachers</NavDropdown.Item>          
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <NavDropdown title="About Us" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/about">About</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/schoolAnthem">School Anthem / Motto</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/contact">Contact Us</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/Login">Login</Nav.Link>       
-            <Nav.Link href="/Gallery">Gallery</Nav.Link>       
+            <Nav.Link as={Link} to="/admission">Admission</Nav.Link>
+            <NavDropdown title="Academics" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/news">News</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/calendar">Academic Calendar</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/events">Events</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/teachers">Our Teachers</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
